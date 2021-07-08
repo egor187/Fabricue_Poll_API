98c7033aac7c48c6879ea52971f3f2e511898e97
egor187 <py.egor.py@gmail.com>
2021-07-08 10:25:03 +0300

    create set of API view for 'poll' app

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Poll, Question, Answer
+from .serializers import PollSerializer, QuestionSerializer, AnswerSerializer, SelfPollSerializer
 
-# Create your views here.
+
+class PollListAPIView(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class PollDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class SelfPollListAPIView(generics.ListAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = SelfPollSerializer
+
+    def get_queryset(self):
+        return Poll.objects.filter(user__id=self.kwargs.get("user_id"))
+
+
+class QuestionListAPIView(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class AnswerListAPIView(generics.ListAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class SelfAnswerListAPIView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+
+    def get_queryset(self):
+        return Answer.objects.filter(user__id=self.kwargs.get("user_id"))
+
+
+class AnswerDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class AnswerCreateAPIView(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+        related_questions_ids = Question.objects.filter(
+            pk=serializer.data.get("question")).values_list("id", flat=True
+                                                            )
+        polls = Poll.objects.filter(questions__id__in=related_questions_ids)
+        for poll in polls:
+            poll.user.add(self.request.user)
+            poll.save()
