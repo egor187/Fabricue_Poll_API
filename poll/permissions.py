c5105265629ac9908b987c95837f7f012ffde4f1
egor187 <py.egor.py@gmail.com>
2021-07-11 14:02:00 +0300

    create custom permission class: only creator of answer get rights to update and delete object (answer)

---
+++ poll/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsAnswerOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow author of an answer to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:  # for GET, HEAD or OPTIONS requests 'read' is granted
+            return True
+        return obj.user == request.user  # for all other (POST, PUT) requests granted only for user, who created object
