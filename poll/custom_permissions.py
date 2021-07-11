0f8363a409569366b67b98635c5b39f9c1691188
egor187 <py.egor.py@gmail.com>
2021-07-11 14:35:13 +0300

    rename module "permissions" and add custom permission class - only creator of answer get rights to retrieve, update and delete object (answer)

--- poll/custom_permissions.py
+++ poll/custom_permissions.py
@@ -9,3 +9,11 @@ class IsAnswerOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:  # for GET, HEAD or OPTIONS requests 'read' is granted
             return True
         return obj.user == request.user  # for all other (POST, PUT) requests granted only for user, who created object
+
+
+class IsAnswerOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow author of an answer to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user  # for all methods requests granted only for user, who created object
